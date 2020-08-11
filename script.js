d88c8bfdd8b9affeefec77371430fd11190e88fe
hrach105 <63844723+hrach105@users.noreply.github.com>
2020-08-12 00:15:28 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-function getPassword() {
-    let chars = "0123456789abscefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !@#$%^&*()_+?><:{}[]";
-    let passwordLength = 16;
-    let password  = "";
-    for(let i = 0; i < passwordLength; i++ ) {
-        let randomNumber = Math.floor(Math.random() * chars.length );
-        password += chars.substring(randomNumber,randomNumber+1);
-    }
-    document.getElementById("password").value = password
-}
+$(document).ready(function() {
+
+	setTimeout(function() {
+		$('#load').hide()
+	},1400)
+
+	setTimeout(function() { 
+		function getPassword() {
+	    let chars = "0123456789abscefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !@#$%^&*()_+?><:{}[]";
+	    let passwordLength = 16;
+	    let password  = "";
+	    for(let i = 0; i < passwordLength; i++ ) {
+	        let randomNumber = Math.floor(Math.random() * chars.length );
+	        password += chars.substring(randomNumber,randomNumber+1);
+	    }
+	    document.getElementById("password").value = password
+		}
+		getPassword() 
+	}, 1500);
+
+	
+})
